9506579bc9854499ac5f92ca9b33a1679f857935
Kirti <kirti.na@daffodilsw.com>
2018-02-08 17:51:06 +0530

    Implemented connectivity check for entire application

--- app/src/main/java/com/disablerouting/DisableRouteApp.java
+++ app/src/main/java/com/disablerouting/DisableRouteApp.java
@@ -6,12 +6,10 @@ import android.content.Context;
 public class DisableRouteApp extends Application {
 
     protected Context mContext = null;
-
     @Override
     public void onCreate() {
         super.onCreate();
         mContext = getApplicationContext();
-
     }
 
 }
---
+++ app/src/main/java/com/disablerouting/NetworkChangeReceiver.java
@@ -0,0 +1,33 @@
+package com.disablerouting;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkChangeReceiver extends BroadcastReceiver {
+
+    private ConnectionChangeListener mConnectionChangeListener;
+
+    public void setConnectionListener(ConnectionChangeListener mConnectionChangeListener) {
+        this.mConnectionChangeListener = mConnectionChangeListener;
+    }
+
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+        if (intent.getExtras() != null) {
+            ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            assert cm != null;
+            NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+            boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+            if (mConnectionChangeListener != null) {
+                mConnectionChangeListener.onNetworkConnectionChanged(isConnected);
+            }
+        }
+    }
+
+    public interface ConnectionChangeListener {
+        void onNetworkConnectionChanged(boolean isConnected);
+    }
+}
