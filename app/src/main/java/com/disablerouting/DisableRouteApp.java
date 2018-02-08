87ef852519e6d23ce7c12ff2de5dcbdfcd592075
Kirti <kirti.na@daffodilsw.com>
2018-02-08 16:38:05 +0530

    Adding application class with permission of internet

---
+++ app/src/main/java/com/disablerouting/DisableRouteApp.java
@@ -0,0 +1,17 @@
+package com.disablerouting;
+
+import android.app.Application;
+import android.content.Context;
+
+public class DisableRouteApp extends Application {
+
+    protected Context mContext = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = getApplicationContext();
+
+    }
+
+}
