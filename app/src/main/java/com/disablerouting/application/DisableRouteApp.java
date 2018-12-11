712096bbdb9116b3257d75d79a105b0fc16e7ecc
Kirti <kirti.na@daffodilsw.com>
2018-12-11 10:41:08 +0530

    Resolving issue of firebase init crash

--- app/src/main/java/com/disablerouting/application/DisableRouteApp.java
+++ app/src/main/java/com/disablerouting/application/DisableRouteApp.java
@@ -1,16 +1,25 @@
 package com.disablerouting.application;
 
-import android.app.Application;
 import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
-public class DisableRouteApp extends Application {
+public class DisableRouteApp extends MultiDexApplication {
 
     protected Context mContext = null;
+
+   @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        mContext = getApplicationContext();
+        MultiDex.install(this);
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
         mContext = getApplicationContext();
+
     }
 
 }
